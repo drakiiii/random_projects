1a68d87641e40c5524a7ee71b81475b94d570ef0
drakiiii <drakonakisa@gmail.com>
2021-01-24 17:16:18 +0000

    First commit

---
+++ josephus.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.9
+
+def josephus_problem(n):
+    x = 0
+    while (2 ** x) < n:
+        x += 1
+    l = n - (2 ** (x-1))
+    w = (2 * l) + 1
+    print(f'Winning seat is {w}')
+    return x
